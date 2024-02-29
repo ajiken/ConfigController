9b7c79a4043e03ff52deaa047a6a4b721298cf04
ajiken <55247883+ajiken@users.noreply.github.com>
2024-02-29 20:35:13 +0900

    Update ConfigController.cs

--- ConfigController.cs
+++ ConfigController.cs
@@ -1,140 +1,80 @@
-﻿using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 
 public class ConfigController
 {
-    public static int GetValueInt(string key)
+    public static T GetValue<T>(string key, Func<string, T> parse, T defaultValue = default)
     {
+        string value = ConfigurationManager.AppSettings[key];
+        
+        if (string.IsNullOrEmpty(value))
+        {
+            Console.WriteLine($"The key: {key} was not found or is empty.");
+            return defaultValue;
+        }
+        
         try
         {
-            string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-            int result = int.Parse(val);
-            return result;
+            return parse(value);
         }
-        catch (ConfigurationErrorsException)
+        catch (Exception ex) when (ex is FormatException || ex is OverflowException)
         {
-            Console.WriteLine($"the key: {key} was not found");
-            Console.WriteLine("Error reading app settings");
-            return 0;
+            Console.WriteLine($"Error parsing the key: {key} with value: {value}");
+            return defaultValue;
         }
     }
 
-    public static double GetValueDouble(string key)
+    public static List<T> GetValueList<T>(string key, Func<string, T> parse)
     {
+        string value = ConfigurationManager.AppSettings[key];
+        
+        if (string.IsNullOrEmpty(value))
+        {
+            Console.WriteLine($"The key: {key} was not found or is empty.");
+            throw new ConfigurationErrorsException("Error reading app settings");
+        }
+        
         try
         {
-            string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-            double result = double.Parse(val);
-            return result;
+            return value.Split(',').Select(parse).ToList();
         }
-        catch (ConfigurationErrorsException)
+        catch (Exception ex) when (ex is FormatException || ex is OverflowException)
         {
-            Console.WriteLine($"the key: {key} was not found");
-            Console.WriteLine("Error reading app settings");
-            return 0;
+            Console.WriteLine($"Error parsing list for the key: {key} with value: {value}");
+            throw;
         }
     }
 
-    public static string GetValueString(string key)
+    public static List<List<T>> GetValueNestedList<T>(string key, Func<string, T> parse)
     {
-        string result = ConfigurationManager.AppSettings[key] ?? "Not Found";
-        if (result == "Not Found")
+        string value = ConfigurationManager.AppSettings[key];
+        
+        if (string.IsNullOrEmpty(value))
         {
-            Console.WriteLine($"the key: {key} was not found");
+            Console.WriteLine($"The key: {key} was not found or is empty.");
             throw new ConfigurationErrorsException("Error reading app settings");
         }
-        return result;
+        
+        try
+        {
+            return value.Split('/')
+                .Select(subList => subList.Split(',').Select(parse).ToList())
+                .ToList();
+        }
+        catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+        {
+            Console.WriteLine($"Error parsing nested list for the key: {key} with value: {value}");
+            throw;
+        }
     }
 
-    public static List<int> GetValueIntList(string key)
-    {
-        string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-        if (val == "Not Found")
-        {
-            Console.WriteLine($"the key: {key} was not found");
-            throw new ConfigurationErrorsException("Error reading app settings");
-        }
-
-        List<int> outputList = new List<int>();
-        foreach (string str in val.Split(","))
-        {
-            outputList.Add(int.Parse(str));
-        }
-        return outputList;
-    }
-
-    public static List<double> GetValueDoubleList(string key)
-    {
-        string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-        if (val == "Not Found")
-        {
-            Console.WriteLine($"the key: {key} was not found");
-            throw new ConfigurationErrorsException("Error reading app settings");
-        }
-
-        List<double> outputList = new List<double>();
-        foreach (string str in val.Split(","))
-        {
-            outputList.Add(double.Parse(str));
-        }
-        return outputList;
-    }
-
-    public static List<List<double>> GetValueDoubleNestedList(string key)
-    {
-        string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-        if (val == "Not Found")
-        {
-            Console.WriteLine($"the key: {key} was not found");
-            throw new ConfigurationErrorsException("Error reading app settings");
-        }
-
-        List<List<double>> outputList = new List<List<double>>();
-        foreach (string splitted_str in val.Split("/"))
-        {
-            List<double> tmpList = new List<double>();
-            foreach (string str in splitted_str.Split(","))
-            {
-                tmpList.Add(double.Parse(str));
-            }
-            outputList.Add(tmpList);
-        }
-        return outputList;
-    }
-
-    public static List<string> GetValueStringList(string key)
-    {
-        string val = ConfigurationManager.AppSettings[key] ?? "Not Found";
-        if (val == "Not Found")
-        {
-            Console.WriteLine($"the key: {key} was not found");
-            throw new ConfigurationErrorsException("Error reading app settings");
-        }
-
-        List<string> outputList = new List<string>();
-        foreach (string str in val.Split(","))
-        {
-            outputList.Add(str);
-        }
-        return outputList;
-    }
-
-    private static List<int> ConvertStringToIntList(string[] strArray)
-    {
-        List<int> outputList = new List<int>();
-        foreach (string str in strArray)
-        {
-            outputList.Add(int.Parse(str));
-        }
-        return outputList;
-    }
-
-    private static List<double> ConvertStringToDoubleList(string[] strArray)
-    {
-        List<double> outputList = new List<double>();
-        foreach (string str in strArray)
-        {
-            outputList.Add(double.Parse(str));
-        }
-        return outputList;
-    }
+    public static int GetValueInt(string key) => GetValue(key, int.Parse, 0);
+    public static double GetValueDouble(string key) => GetValue(key, double.Parse, 0.0);
+    public static string GetValueString(string key) => GetValue(key, str => str, "Not Found");
+    public static List<int> GetValueIntList(string key) => GetValueList(key, int.Parse);
+    public static List<double> GetValueDoubleList(string key) => GetValueList(key, double.Parse);
+    public static List<string> GetValueStringList(string key) => GetValueList(key, str => str);
+    public static List<List<double>> GetValueDoubleNestedList(string key) => GetValueNestedList(key, double.Parse);
 }
